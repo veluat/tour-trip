9c09de4d146c63c27878a0df83bdb46a4c9edcf8
Julia <veluat@gmail.com>
2026-08-01 23:36:43 +0300

    refactor: update imports to use @modules alias

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import { initBurger } from './modules/burger.js';
-import { initDropdowns } from './modules/dropdown.js';
-import { initSchedule } from './modules/schedule.js';
-import { initSlider } from './modules/slider.js';
-import { initReviews } from './modules/reviews.js';
+import { initBurger } from '@modules/burger.js';
+import { initDropdowns } from '@modules/dropdown.js';
+import { initSchedule } from '@modules/schedule.js';
+import { initSlider } from '@modules/slider.js';
+import { initReviews } from '@modules/reviews.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initBurger();
